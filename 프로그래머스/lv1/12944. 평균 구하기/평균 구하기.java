79df77070e37b4caa30baf3b0e437d73d76ce43f
TofuMagician <130903086+TofuMagician@users.noreply.github.com>
2023-08-01 19:00:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        for(int x : arr){
+            
+            answer += x;
+        }
+        
+        answer /= arr.length; 
+        
+        return answer;
+    }
+}
