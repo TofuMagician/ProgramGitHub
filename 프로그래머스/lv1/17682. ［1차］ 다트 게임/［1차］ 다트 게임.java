5cac7510b046bbc92c8c9c29948aad5cce31d325
TofuMagician <130903086+TofuMagician@users.noreply.github.com>
2023-08-23 16:50:23 +0900

    [level 1] Title: [1차] 다트 게임, Time: 6.21 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class Solution {
+    
+    public int funcMath(String sentence){
+        
+        if(sentence.equals("*")) return -2;
+        else if(sentence.equals("#")) return -1;
+        else{
+            sentence = sentence.replace("S", "mS");
+            sentence = sentence.replace("D", "mD");
+            sentence = sentence.replace("T", "mT");
+            
+            String[] word = sentence.split("m");
+            
+            int num = Integer.parseInt(word[0]);
+            
+            if(word[1].equals("S")) return num;
+            if(word[1].equals("D")) return num*num;
+            if(word[1].equals("T")) return num*num*num;
+        }
+        
+        return 0;
+    }
+    
+    public int solution(String dartResult) {
+        int answer = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        dartResult = dartResult.replace("*", "*m");
+        dartResult = dartResult.replace("S", "Sm");
+        dartResult = dartResult.replace("D", "Dm");
+        dartResult = dartResult.replace("T", "Tm");
+        dartResult = dartResult.replace("#", "#m");
+        
+        String[] sentence = dartResult.split("m");
+        
+        int[] num = new int[sentence.length];
+        
+        for(int i = 0; i < sentence.length; i++){ 
+            
+            num[i] = funcMath(sentence[i]);
+            
+            System.out.print(num[i]+ " ");
+            
+        }
+        int level = 0;
+        
+        for(int t : num){
+            
+            stack.push(t);
+            
+            if(t == -1){
+                stack.pop();
+                stack.push(stack.pop() * -1);
+            }
+            
+            if(t == -2){
+                stack.pop();
+                if(level == 1){
+                    stack.push(stack.pop() * 2);
+                }
+                else{
+                    int temp = stack.pop();
+                    stack.push(stack.pop() * 2);
+                    stack.push(temp * 2);
+                }
+            }
+            
+            level++;
+        }
+        
+        System.out.println();
+      
+        while(!stack.isEmpty()){
+            System.out.print(stack.peek() +" ");
+            answer += stack.pop();
+        }
+        
+        return answer;
+    }
+}
