e66c54fdcc1b5a3db7e9427f4b7ba1698c359cc0
TofuMagician <130903086+TofuMagician@users.noreply.github.com>
2023-09-10 17:39:06 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner text = new Scanner(System.in);
+        
+        int a = text.nextInt();
+        int b = text.nextInt();
+        
+        int c = a + b;
+        
+        System.out.print(c);
+    }
+}
